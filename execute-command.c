ef3c7e56a34000a4822a0ffc82457fe5b49cc86b
tsleeve <akash5malik@gmail.com>
2015-04-20 16:41:39 -0700

    testing

--- execute-command.c
+++ execute-command.c
@@ -17,9 +17,6 @@ command_status (command_t c)
 void
 execute_command (command_t c, bool time_travel)
 {
-  /* FIXME: Replace this with your implementation.  You may need to
-     add auxiliary functions and otherwise modify the source code.
-     You can also use external functions defined in the GNU C Library.  */
   time_travel = false;
   c->type = PIPE_COMMAND; 
 }

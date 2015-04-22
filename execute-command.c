3c8a137955531520d5e0264b09b89f163f46d457
Luke Harmon <vellottiwizard@gmail.com>
2015-04-22 07:48:58 -0700

    Added Pipe

--- execute-command.c
+++ execute-command.c
@@ -132,7 +132,48 @@ void execute_subshell(command_t c, bool time_travel)
     }
 }
 
-
+void execute_pipe(command_t c, bool time_travel)
+{
+  int fd[2];
+  pipe(fd);
+  pid_t firstPid = fork();
+  if (firstPid == 0) // first child
+    {
+      close(fd[1]);
+      dup2(fd[0],0);
+      execute_command(c->u.command[1], time_travel);
+      int st = c->u.command[1]->status;
+      if (st == 0)
+	exit(0);
+      else
+	exit(1);
+    }
+  else
+    {
+      pid_t secondPid = fork();
+      if (secondPid == 0)
+	{
+	  close(fd[0]);
+	  dup2(fd[1],1);
+	  execute_command(c->u.command[0], time_travel);
+	  int st = c->u.command[0]->status;
+	  if (st == 0)
+	    exit(0);
+	  else
+	    exit(1);
+	}
+      else
+	{
+	  close(fd[0]);
+	  close(fd[1]);
+	  int status;
+	  waitpid(-1, &status, 0);
+	  waitpid(-1, &status, 0);
+	  int estatus = WEXITSTATUS(status);
+	  c->status = estatus;
+	}
+    }
+}
 void
 execute_command (command_t c, bool time_travel)
 {
@@ -140,13 +181,15 @@ execute_command (command_t c, bool time_travel)
   switch(c->type)
   {
 	case SIMPLE_COMMAND:
-			execute_simple(c); break;
+	  execute_simple(c); break;
 	case OR_COMMAND:
 	case AND_COMMAND:
 	  execute_andor(c, time_travel);	break;
 	case SEQUENCE_COMMAND:
-			execute_seq(c,time_travel);break;
-	case PIPE_COMMAND: 	break;
-  case SUBSHELL_COMMAND:	execute_subshell(c, time_travel); break;
+	  execute_seq(c,time_travel);break;
+        case PIPE_COMMAND: 	
+	  execute_pipe(c, time_travel); break;
+        case SUBSHELL_COMMAND:	
+	  execute_subshell(c, time_travel); break;
   }
 }

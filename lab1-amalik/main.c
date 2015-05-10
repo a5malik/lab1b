f0a5105e243ef92dd4339c57d9ff124e8674c2d0
Luke Harmon <vellottiwizard@gmail.com>
2015-05-10 15:03:40 -0600

    Parses argument for N

--- lab1-amalik/main.c
+++ lab1-amalik/main.c
@@ -4,7 +4,9 @@
 #include <error.h>
 #include <getopt.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #include "command.h"
 
 static char const *program_name;
@@ -13,7 +15,7 @@ static char const *script_name;
 static void
 usage (void)
 {
-  error (1, 0, "usage: %s [-pt] SCRIPT-FILE", program_name);
+  error (1, 0, "usage: %s [-pt] [-n NUMBER] SCRIPT-FILE", program_name);
 }
 
 static int
@@ -27,56 +29,90 @@ main (int argc, char **argv)
 {
   int command_number = 1;
   bool print_tree = false;
-  bool time_travel = true;
+  bool time_travel = false;
+  bool hasN = false;
   program_name = argv[0];
-
+  int *N = NULL;
+  //  char *c;
+  int i = 0;
   for (;;)
-    switch (getopt (argc, argv, "pt"))
-      {
-      case 'p': print_tree = true; break;
-      case 't': time_travel = true; break;
-      default: usage (); break;
-      case -1: goto options_exhausted;
-      }
+    {
+      switch (getopt (argc, argv, "ptn0123456789"))
+	{
+	case 'p': print_tree = true; i--; break;
+	case 't': time_travel = true; i--; break;
+	case 'n': hasN = true; break;
+	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9': break;
+	default: usage (); break;
+	case -1: goto options_exhausted;
+	}
+      i++;
+    }
  options_exhausted:;
-
   // There must be exactly one file argument.
-  if (optind != argc - 1)
+  if (hasN)
+    optind++;
+  if (optind != (argc - 1))
     usage ();
-
+  if (hasN)
+    {
+      for (i = 0; i < argc; i++)
+	{
+	  if (argv[i][0] == '-' && argv[i][strlen(argv[i])-1] == 'n')
+	    break;
+	}
+      N = (int *)malloc(sizeof(int));
+      *N = 0;
+      unsigned int j;
+      for (j = 0; j < strlen(argv[i+1]); j++)
+	{
+	  if (!(argv[i+1][j] >= '0' && argv[i+1][j] <= '9'))
+	    usage();
+	  *N += (argv[i+1][j] - '0') * pow(10, strlen(argv[i+1])-j-1);
+	}
+    }
   script_name = argv[optind];
   FILE *script_stream = fopen (script_name, "r");
   if (! script_stream)
     error (1, errno, "%s: cannot open", script_name);
-  command_stream_t command_stream =
-    make_command_stream (get_next_byte, script_stream);
+    command_stream_t command_stream =
+      make_command_stream (get_next_byte, script_stream);
 
-  command_t last_command = NULL;
-  command_t command;
-  if(time_travel)
-  {
-	  DependencyGraph graph;
-	  createGraph(command_stream,&graph);
-	  int fstatus = 0;
-	  executeGraph(&graph);
-	  return fstatus;
-  }
-  else
-  {
-  while ((command = read_command_stream (command_stream)))
-    {
-      if (print_tree)
-	{
-	  printf ("# %d\n", command_number++);
-	  print_command (command);
-	}
-      else
-	{
-	  last_command = command;
-	  execute_command (command, time_travel);
-	}
-    }
+    command_t last_command = NULL;
+    command_t command;
+    if(time_travel)
+      {
+	DependencyGraph graph;
+	createGraph(command_stream,&graph);
+	int fstatus = 0;
+	executeGraph(&graph, N);
+	return fstatus;
+      }
+    else
+      {
+	while ((command = read_command_stream (command_stream)))
+	  {
+	    if (print_tree)
+	      {
+		printf ("# %d\n", command_number++);
+		print_command (command);
+	      }
+	    else
+	      {
+		last_command = command;
+		execute_command (command, time_travel);
+	      }
+	  }
 
-  return print_tree || !last_command ? 0 : command_status (last_command);
-  }
+	return print_tree || !last_command ? 0 : command_status (last_command);
+      }
 }
